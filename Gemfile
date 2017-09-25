7424611732e32d6ce23c25b85ab52fd450685a86
yamamoto.taro <aaaaaa@email.co.jp>
2017-09-25 14:18:36 +0000

    lesson10 create bootstrap design

--- Gemfile
+++ Gemfile
@@ -53,3 +53,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'pry-byebug', group: :development
+gem 'kaminari'
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(created_at: :desc).page(params[:page]).per(5)
   end
   
   def show
