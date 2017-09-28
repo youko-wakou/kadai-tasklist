8cc5cfa86321de0466986ff777a7efbc316c6a77
yamamoto.taro <aaaaaa@email.co.jp>
2017-09-28 14:25:39 +0000

    lesson12 signup_user and login_user tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all.order(created_at: :desc).page(params[:page]).per(5)
+    
+      # @tasks = current_user.tasks.all.order(created_at: :desc).page(params[:page]).per(5)
+      @tasks = current_user.tasks.order(created_at: :desc).page(params[:page]).per(5)
   end
   
   def show
@@ -15,13 +19,13 @@ class TasksController < ApplicationController
   end
   
   def create
-    # binding.pry
-    @task = Task.new(task_params)
-    
+   
+    @task = current_user.tasks.new(task_params)
     if @task.save
       flash[:success] = 'Task　が正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -60,7 +64,14 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content, :status,:user_id)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
 end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  
+  before_save {self.email.downcase! }
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                  format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                  uniqueness: { case_sensitive: false }
+                  
+  has_secure_password
+  has_many :tasks, class_name: "Task", foreign_key: "user_id"
+  # belongs_to :task
+  
+  
+ 
+end
